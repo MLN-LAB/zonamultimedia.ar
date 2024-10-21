11d69b34a3c94b6d51cce1c2d451bddf712eaca7
MLN-LAB <MAXINARDI@HOTMAIL.COM>
2024-10-21 10:44:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("mobile-menu"),t=document.getElementById("nav-list"),n=document.querySelectorAll(".section"),a=document.querySelectorAll(".nav-list a");e.addEventListener("click",()=>t.classList.toggle("active")),a.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();n.forEach(e=>e.classList.remove("active"));document.getElementById(e.id.replace("-link","")).classList.add("active"),t.classList.remove("active")})})});
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.getElementById('nav-list');
+
+    mobileMenu.addEventListener('click', function () {
+        navList.classList.toggle('active'); // Alternar la clase 'active' para mostrar/ocultar el menú
+    });
+
+    const sections = document.querySelectorAll('.section');
+    const navLinks = document.querySelectorAll('.nav-list a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.id.replace('-link', '');
+            sections.forEach(section => {
+                section.classList.remove('active');
+            });
+            document.getElementById(targetId).classList.add('active');
+            navList.classList.remove('active'); // Ocultamos el menú al hacer clic en un enlace
+        });
+    });
+});
